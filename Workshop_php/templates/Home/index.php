4d0285d61d49af0e6e54643e66a73f1a7c142690
LAURET Benjamin <benjamin.lauret@epitech.eu>
2023-06-22 11:32:47 +0400

    first_commit

---
+++ Workshop_php/templates/Home/index.php
@@ -0,0 +1,4 @@
+<html>
+    <h1>Workshop_PHP</h1>
+</html>
+<?= $this->Html->link('Pages')?>
---
+++ Workshop_php/templates/layout/my_Layout.php
@@ -0,0 +1,16 @@
+<?= $this->Html->css("home2.css")?>
+<main class="main">
+    <div class="container">
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+        <div class="first">
+            <h1>
+                Workshop_PHP
+            </h1>
+        </div>
+    </div>
+</main>
